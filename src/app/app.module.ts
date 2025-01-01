255cc2654ed0c2c7cf9d71ef774eb0b742cb6a0e
akshayibitwar <akshayebitwar@gmail.com>
2025-01-01 13:26:40 +0530

    User LogIn-Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogInFormComponent } from './shared/components/log-in-form/log-in-form.component';
+import { SignInFormComponent } from './shared/components/sign-in-form/sign-in-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInFormComponent,
+    SignInFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
